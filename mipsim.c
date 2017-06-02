1debcab78c361ce7befb179db524a863f5ce59d2
Kenji Hisazumi <kenji@hisazumi.jp>
2017-06-02 19:44:05 +0900

    refactored

--- mipsim.c
+++ mipsim.c
@@ -1,60 +1,11 @@
 #include <stdio.h>
 
 /*------------------------------------------------------------*/
-/* MIPS like Instruction Set
+/* MIPS like Processor Structure
  */
-
-enum {
-    ADDI=1, ADD, SLT, BNE, BEQ, LW, SW, SRL, SLL
-};
-
-#define R(op, rs, rt, rd) ((op << (32-6)) | (rs << (32-6-5)) | (rt << (32-6-5-5)) | (rd << (32-6-5-5-5)))
-#define I(op, rs, rt, im) ((op << (32-6)) | (rs << (32-6-5)) | (rt << (32-6-5-5)) | (im & 0xffff))
-
-#define OP(inst) ((inst >> (32-6)) & 0x3f)
-#define RS(inst) ((inst >> (32-6-5)) & 0x1f)
-#define RT(inst) ((inst >> (32-6-5-5)) & 0x1f)
-#define RD(inst) ((inst >> (32-6-5-5-5)) & 0x1f)
-#define IM(inst) (inst & 0x0000ffff)
-
-#define ADD(rd, rs, rt)  R(ADD,  rs, rt, rd)
-#define ADDI(rt, rs, im) I(ADDI, rs, rt, im)
-#define BNE(rs, rt, im) I(BNE, rs, rt, im)
-#define BEQ(rs, rt, im) I(BEQ, rs, rt, im)
-#define SLT(rd, rs, rt) R(SLT, rs, rt, rd)
-#define LW(rt, im, rs) I(LW, rs, rt, im)
-#define SW(rt, im, rs) I(SW, rs, rt, im)
-#define SRL(rs, rt, im) I(SRL, rs, rt, im)
-#define SLL(rs, rt, im) I(SLL, rs, rt, im)
-
-/*------------------------------------------------------------*/
-/* A sample code
- */
-
-int inst_mem[] = {
-        ADDI(0, 0, 0), //0: 0
-        ADDI(1, 0, 1), //1: counter
-        ADDI(2, 0, 0), //2; sum
-        ADDI(3, 0, 1), //3: 1
-        ADDI(4, 0, 10001), //4: 10001
-        ADD(2, 2, 1), //9: r2 += r1
-        ADD(1, 1, 3), //10: r1 += r3
-        BNE(1, 4, 5), //11: if (counter != r3) => jmp 9
-        0 //8: terminator
-};
 
 static int pc = 0;
 static int regs[32] = {0};
-
-void dump (void) {
-    printf ("==========\n");
-    printf ("pc: %d\n", pc);
-    printf ("reg: ");
-    for (int i = 0; i < 10; i++) {
-        printf ("%x ", regs[i]);
-    }
-    printf ("\n");
-}
 
 int reg_get (int n) {
     if (n == 0) {
@@ -75,16 +26,6 @@ void reg_set (int n, int value) {
         printf ("invalid register write: %d\n", n);
     }
 }
-
-int fetch (int addr) {
-    if (0 <= addr && addr < sizeof(inst_mem)/sizeof(inst_mem[0])){
-        return inst_mem[pc];
-    }else{
-        printf ("invalid memory access %d\n", addr);
-        return 0;
-    }
-}
-
 static int data_mem[100] = {0};
 int is_mem_addr_valid (int addr) {
     return 0 <= addr && addr <= sizeof(data_mem)/sizeof(data_mem[0]);
@@ -107,6 +48,129 @@ void mem_set (int addr, int value) {
     }
 }
 
+
+/*------------------------------------------------------------*/
+/* MIPS like Instruction Set
+ */
+
+// Macros for decode
+#define OP(inst) ((inst >> (32-6)) & 0x3f)
+#define RS(inst) ((inst >> (32-6-5)) & 0x1f)
+#define RT(inst) ((inst >> (32-6-5-5)) & 0x1f)
+#define RD(inst) ((inst >> (32-6-5-5-5)) & 0x1f)
+#define IM(inst) (inst & 0x0000ffff)
+
+enum {
+    ADDI=1, ADD, SLT, BNE, BEQ, LW, SW, SRL, SLL
+};
+
+#define R(op, rs, rt, rd) ((op << (32-6)) | (rs << (32-6-5)) | (rt << (32-6-5-5)) | (rd << (32-6-5-5-5)))
+#define I(op, rs, rt, im) ((op << (32-6)) | (rs << (32-6-5)) | (rt << (32-6-5-5)) | (im & 0xffff))
+
+#define DEFINST(name) void name (int rs, int rt ,int rd, int im)
+
+DEFINST(nop) {
+}
+
+#define ADD(rd, rs, rt)  R(ADD,  rs, rt, rd)
+DEFINST(add) {
+    reg_set (rd, reg_get (rs) + reg_get (rt));
+}
+
+#define ADDI(rt, rs, im) I(ADDI, rs, rt, im)
+DEFINST(addi) {
+    reg_set (rt, reg_get (rs) + im);
+}
+
+#define BNE(rs, rt, im) I(BNE, rs, rt, im)
+DEFINST(bne) {
+    // PC相対アドレッシングではなくて，直接アドレッシング
+    if (reg_get (rs) != reg_get (rt)) {
+        pc = im;
+    }
+}
+
+#define BEQ(rs, rt, im) I(BEQ, rs, rt, im)
+DEFINST (beq) {
+    // PC相対アドレッシングではなくて，直接アドレッシング
+    if (reg_get (rs) == reg_get (rt)) {
+        pc = im;
+    }
+}
+
+#define SLT(rd, rs, rt) R(SLT, rs, rt, rd)
+DEFINST (slt) {
+    reg_set (rd, reg_get (rs) + reg_get (rt));
+}
+
+#define LW(rt, im, rs) I(LW, rs, rt, im)
+DEFINST (lw) {
+    reg_set (rt, mem_get (rs + im));
+}
+
+#define SW(rt, im, rs) I(SW, rs, rt, im)
+DEFINST (sw) {
+    mem_set (rs + im, reg_get (rt));
+}
+
+#define SRL(rs, rt, im) I(SRL, rs, rt, im)
+DEFINST (srl) {
+    reg_set (rs, reg_get (rt) >> im);
+}
+
+#define SLL(rs, rt, im) I(SLL, rs, rt, im)
+DEFINST (sll) {
+    reg_set (rs, reg_get (rt) << im);
+}
+
+void (*instructions[])(int, int, int, int) = {
+    0, addi, add, slt, bne, beq, lw, sw, srl, sll
+};
+
+/*------------------------------------------------------------*/
+/* A sample code
+ */
+
+int inst_mem[] = {
+        ADDI(0, 0, 0), //0: 0
+        ADDI(1, 0, 1), //1: counter
+        ADDI(2, 0, 0), //2; sum
+        ADDI(3, 0, 1), //3: 1
+        ADDI(4, 0, 10001), //4: 10001
+        ADD(2, 2, 1), //9: r2 += r1
+        ADD(1, 1, 3), //10: r1 += r3
+        BNE(1, 4, 5), //11: if (counter != r3) => jmp 9
+        0 //8: terminator
+};
+
+void dump (void) {
+    printf ("==========\n");
+    printf ("pc: %d\n", pc);
+    printf ("reg: ");
+    for (int i = 0; i < 10; i++) {
+        printf ("%x ", regs[i]);
+    }
+    printf ("\n");
+}
+
+int fetch (int addr) {
+    if (0 <= addr && addr < sizeof(inst_mem)/sizeof(inst_mem[0])){
+        return inst_mem[pc];
+    }else{
+        printf ("invalid memory access %d\n", addr);
+        return 0;
+    }
+}
+
+void execute (int op, int rs, int rt, int rd, int im) {
+    pc++;
+    if (0 <= op && op <= sizeof(instructions)/sizeof(instructions[0])) {
+        instructions [op](rs, rt, rd, im);
+    } else {
+        printf ("invalid op: %x\n", op);
+    }    
+}
+
 int main() {
     while (1) {
         int inst;
@@ -117,64 +181,17 @@ int main() {
             return 0;
         }
 
-        {
-            // decode
-            int rs = RS(inst);
-            int rt = RT(inst);
-            int rd = RD(inst);
-            int im = IM(inst);
+        // decode
+        int op = OP(inst);
+        int rs = RS(inst);
+        int rt = RT(inst);
+        int rd = RD(inst);
+        int im = IM(inst);
 
-            // execute
-            switch (OP(inst)) {
-            case ADDI:
-                reg_set (rt, reg_get (rs) + im);
-                pc++;
-                break;
-            case ADD:
-                reg_set (rd, reg_get (rs) + reg_get (rt));
-                pc++;
-                break;
-            case SLT:
-                reg_set (rd, reg_get (rs) + reg_get (rt));
-                pc++;
-                break;
-            case BNE:
-                if (reg_get (rs) != reg_get (rt)) {
-                    // PC相対アドレッシングはやらない
-                    pc = im;
-                }else{
-                    pc++;
-                }
-                break;
-            case BEQ:
-                if (reg_get (rs) != reg_get (rt)) {
-                    // PC相対アドレッシングはやらない
-                    pc = im;
-                }else{
-                    pc++;
-                }
-                break;
-            case LW:
-                reg_set (rt, mem_get (rs + im));
-                pc++;
-                break;
-            case SW:
-                mem_set (rs + im, reg_get (rt));
-                pc++;
-                break;
-            case SRL:
-                reg_set (rs, reg_get (rt) >> im);
-                pc++;
-                break;
-            case SLL:
-                reg_set (rs, reg_get (rt) << im);
-                pc++;
-                break;
-            default:
-                printf ("invalid op: %x (in %x)\n", OP(inst), inst);
-            }
-        }
+        // execute
+        execute (op, rs, rt, rd, im);
 
+        // print status
         dump ();
     }
 
